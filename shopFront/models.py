64c655fdd3b23c0751e2b25f037ca91b5d67c714
mahmoud <mahmoudaljaziri77gmail.com>
2024-05-06 22:01:27 +0300

    fixed the image upload problem

--- shopFront/models.py
+++ shopFront/models.py
@@ -61,7 +61,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
+    profile_pic = models.FileField(default="profile1.png", null=True, blank=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=6,choices=MEMPERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
